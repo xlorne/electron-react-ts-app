0965e2e6c2b2162a96a2ef333d8b8c35bd820df8
lorne <1991wangliang@gmail.com>
2024-12-22 19:23:31 +0800

    add asset

--- webpack.renderer.config.ts
+++ webpack.renderer.config.ts
@@ -4,6 +4,7 @@ import {rules} from './webpack.rules';
 import {plugins} from './webpack.plugins';
 import path from "path";
 
+
 // 配置静态资源处理
 rules.push(
     {
@@ -16,7 +17,36 @@ rules.push(
     {
         test: /\.(png|jpg|jpeg|gif|svg)$/i,
         type: 'asset/resource',
+        generator: {
+            filename: 'images/[name][ext]'
+        }
     },
+    // 字体文件
+    {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+            filename: 'fonts/[name][ext]'
+        }
+    },
+    // 媒体文件
+    {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)$/i,
+        type: 'asset/resource',
+        generator: {
+            filename: 'media/[name][ext]'
+        }
+    },
+    // 小文件内联处理
+    {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        type: 'asset',
+        parser: {
+            dataUrlCondition: {
+                maxSize: 8 * 1024 // 8kb
+            }
+        }
+    }
 );
 
 export const rendererConfig: Configuration = {
@@ -34,3 +64,4 @@ export const rendererConfig: Configuration = {
         assetModuleFilename: 'assets/[name][ext]',
     }
 };
+
