b16387ca94eee2b40519802ae5a40365f8801ad6
lorne <1991wangliang@gmail.com>
2024-12-23 10:49:00 +0800

    add http

--- src/server/controller/http.ts
+++ src/server/controller/http.ts
@@ -3,32 +3,61 @@ import {net} from "electron";
 
 export class HttpController extends EventController {
 
-    private readonly bastUrl: string = 'http://localhost:8090';
+    private readonly baseUrl: string = 'http://localhost:8090';
+
+    private token:string;
+
+    private handlerResponse = async (response: any) => {
+        const authorization = response.headers['authorization'];
+        if (authorization) {
+            this.token = authorization;
+        }
+        return await response.json();
+    }
+
 
     public post() {
         super.registerHandler('post', async (event, args: { url: string, body: any }) => {
-            const url = `${this.bastUrl}${args.url}`;
-            const res = await net.fetch(url, {
-                method: 'POST',
-                body: JSON.stringify(args.body),
-                headers: {
-                    'Content-Type': 'application/json'
+            const url = `${this.baseUrl}${args.url}`;
+            try {
+                const res = await net.fetch(url, {
+                    method: 'POST',
+                    body: JSON.stringify(args.body),
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': this.token
+                    },
+                });
+                return this.handlerResponse(res);
+            } catch (e) {
+                return {
+                    success: false,
+                    errCode: 'fetch.error',
+                    errMessage: e.message
                 }
-            });
-            return res.json();
+            }
         });
     }
 
     public get() {
         super.registerHandler('get', async (event, args: { url: string, params: any }) => {
-            const url = `${this.bastUrl}${args.url}`;
-            const res =  await net.fetch(url, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json'
+            const url = `${this.baseUrl}${args.url}`;
+            try {
+                const res = await net.fetch(url, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': this.token
+                    }
+                });
+                return this.handlerResponse(res);
+            }catch (e) {
+                return {
+                    success: false,
+                    errCode: 'fetch.error',
+                    errMessage: e.message
                 }
-            });
-            return res.json();
+            }
         });
     }
 
