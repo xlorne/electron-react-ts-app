57941b424847ccfd44858b851dd7a0a107b8f86c
lorne <1991wangliang@gmail.com>
2025-01-14 11:10:32 +0800

    locateFile

--- src/server/utils/media.ts
+++ src/server/utils/media.ts
@@ -1,28 +1,16 @@
 import MediaInfoFactory from 'mediainfo.js'
 import {FileUtils} from "@/server/utils/file";
-import {app} from 'electron';
-import * as nodePath from 'node:path';
 
 export class MediaUtils {
 
     public static loadMediaInfo = async (file: string) => {
         return new Promise((resolve, reject) => {
             MediaInfoFactory({
+                //This method will be called before loading the WASM file. It should return the actual URL
                 locateFile:(path, prefix)=>{
-                    console.log('prefix', prefix);
-                    console.log('path', path);
-
                     if (path.endsWith('.wasm')) {
-                        // 在主进程中使用绝对路径
-                        if (process.type === 'browser') {
-                            const wasmPath = app.isPackaged
-                                ? nodePath.join(process.resourcesPath, '.webpack/main/assets', path)
-                                : nodePath.join(__dirname, 'assets', path);
-                            console.log('wasmPath', wasmPath);
-                            return 'file:///Users/lorne/Downloads/MediaInfoModule.wasm';
-                        }
-                        // 在渲染进程中使用相对路径
-                        return `assets/${path}`;
+                        // 必须替换成一个http的地址才行
+                        return 'http://localhost:8090/' + path;
                     }
                     return prefix + path;
                 }
